93ed7e649c7aa67637c0b213000136fd53df20f2
unknown <kalusam619@gmail.com>
2022-05-14 10:36:51 +0100

    feat:added a testing feature

---
+++ main.py
@@ -0,0 +1,22 @@
+
+import cv2
+import matplotlib.pyplot as plt
+from tensorflow import keras
+import numpy as np
+model = keras.models.load_model('./digit_classification.h5')
+
+image_path = 'test_images/img_1.png'
+im = cv2.imread(image_path)
+im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+im = cv2.resize(im, (28,28))
+
+#####normalize data
+im = im/255
+im = np.array(im)
+print(im)
+
+prediction = model.predict(im)
+print(prediction)
+cv2.imshow('Image', im)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
