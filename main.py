b805495d9409caa3321f9aa7cc032098501abe51
Samuel <kalusam619@gmail.com>
2022-05-15 20:12:56 +0100

    feat:made few final changes

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 
 import cv2
 import matplotlib.pyplot as plt
-from tensorflow import keras
 import numpy as np
+
+
+from tensorflow import keras
 model = keras.models.load_model('./digit_classification.h5')
 
-image_path = 'test_images/img_3.png'
+print(model.summary())
+image_path = 'test_images/img_0.PNG'
 im = cv2.imread(image_path)
 im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 im = cv2.resize(im, (28, 28))
+cv2.imshow('Number', im)
 
-
-print("Model Summary")
-print(model.summary())
-
-# normalize data
+######### NORMALIZE DATA
 im = im/255
 im = np.array(im)
 print('The array is ', end='')
@@ -22,5 +22,12 @@ print(im.shape)
 print(im)
 im = im.reshape(-1, 28, 28, 1)
 prediction = model.predict(im)
-print("The number is", np.argmax(prediction))
 print(prediction)
+print()
+print("The number is:", np.argmax(prediction))
+
+# TODO: The model itself is making wrong predictions on the new test data , and i think 
+# I'll have to work on that sometime later when i gain more indepth knowledge
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
