340c10d33c226af628275a3ddc93b0a8a2f8354d
unknown <kalusam619@gmail.com>
2022-05-15 11:22:06 +0100

    fixed crazy bug

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from tensorflow import keras
 import numpy as np
 model = keras.models.load_model('./digit_classification.h5')
 
-image_path = 'test_images/img_1.png'
+image_path = 'test_images/img_3.png'
 im = cv2.imread(image_path)
 im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 im = cv2.resize(im, (28, 28))
+
+
+print("Model Summary")
+print(model.summary())
 
 # normalize data
 im = im/255
@@ -16,8 +20,7 @@ im = np.array(im)
 print('The array is ', end='')
 print(im.shape)
 print(im)
-prediction = model.predict([im])
+im = im.reshape(-1, 28, 28, 1)
+prediction = model.predict(im)
+print("The number is", np.argmax(prediction))
 print(prediction)
-cv2.imshow('Image', im)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
